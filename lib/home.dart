74fa51b18171c415a306fc991daec63476b5b782
Akbar Nuriddinov <82442697+Akbar1813@users.noreply.github.com>
2022-06-29 17:01:56 +0500

    Update home.dart
    bug fixed

--- lib/home.dart
+++ lib/home.dart
@@ -8,13 +8,36 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
   double height = 100;
 
   void changeHeight(double height) {
     setState(() {
       this.height = height;
     });
+  }
+
+  void handleTabSelection() {
+    switch (tabController.index) {
+      case 0:
+        changeHeight(100);
+        break;
+      case 1:
+        changeHeight(150);
+        break;
+      default:
+        changeHeight(200);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+    tabController.addListener(handleTabSelection);
   }
 
   @override
@@ -29,62 +52,61 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             children: [
               for (int i = 0; i < 2; i++) ...{TopWidget()},
-              DefaultTabController(
-                length: 3,
-                child: Column(
-                  children: [
-                    Container(
-                      height: 100,
-                      color: Colors.black,
-                      child: TabBar(
-                        onTap: (index) {
-                          if (index == 0) {
-                            changeHeight(100);
-                            return;
-                          }
-                          if (index == 1) {
-                            changeHeight(150);
-                            return;
-                          }
-                          changeHeight(200);
-                        },
-                        tabs: [
-                          Tab(
-                            icon: Text('TAB 1'),
-                          ),
-                          Tab(
-                            icon: Text('TAB 2'),
-                          ),
-                          Tab(
-                            icon: Text('TAB 3'),
-                          ),
-                        ],
-                      ),
+              Column(
+                children: [
+                  Container(
+                    height: 100,
+                    color: Colors.black,
+                    child: TabBar(
+                      controller: tabController,
+                      onTap: (index) {
+                        if (index == 0) {
+                          changeHeight(100);
+                          return;
+                        }
+                        if (index == 1) {
+                          changeHeight(150);
+                          return;
+                        }
+                        changeHeight(200);
+                      },
+                      tabs: [
+                        Tab(
+                          icon: Text('TAB 1'),
+                        ),
+                        Tab(
+                          icon: Text('TAB 2'),
+                        ),
+                        Tab(
+                          icon: Text('TAB 3'),
+                        ),
+                      ],
                     ),
-                    Container(
-                      height: height,
-                      child: TabBarView(
-                        children: [
-                          Container(
-                            child: Center(child: Text('tab 1  height: 100')),
-                            color: Colors.red,
-                          ),
-                          Container(
-                            child: Center(child: Text('tab 2  height: 150')),
-                            color: Colors.orange,
-                          ),
-                          Container(
-                            child: Center(child: Text('tab 3  height: 200')),
-                            color: Colors.yellow,
-                          )
-                        ],
-                      ),
+                  ),
+                  Container(
+                    height: height,
+                    child: TabBarView(
+                      controller: tabController,
+                      children: [
+                        Container(
+                          child: Center(child: Text('tab 1  height: 100')),
+                          color: Colors.red,
+                        ),
+                        Container(
+                          child: Center(child: Text('tab 2  height: 150')),
+                          color: Colors.orange,
+                        ),
+                        Container(
+                          child: Center(child: Text('tab 3  height: 200')),
+                          color: Colors.yellow,
+                        )
+                      ],
                     ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
               ),
               for (int i = 0; i < 4; i++) ...{TopWidget()},
             ],
